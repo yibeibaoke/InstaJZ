e6bb32ff6eec5bcc4462285d47e389fa0c54c77e
WenTailai <wentailai.jiuzhang@gmail.com>
2019-08-03 20:08:33 -0400

    end of first day

--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
