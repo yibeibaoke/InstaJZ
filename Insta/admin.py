8de92043d2104800d3d73e4e52f05cf044027e45
WenTailai <wentailai.jiuzhang@gmail.com>
2019-08-04 18:41:05 -0400

    fix index styles

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from Insta.models import Post, InstaUser
+from Insta.models import InstaUser, Like, Post
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(InstaUser)
+admin.site.register(Like)
--- Insta/models.py
+++ Insta/models.py
@@ -6,7 +6,21 @@ from django.urls import reverse
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to='static/images/profiles',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+        )
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to='static/images/posts',
@@ -16,15 +30,25 @@ class Post(models.Model):
         null=True
         )
 
+    def get_like_count(self):
+        return self.likes.count()
+
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
-    
 
-class InstaUser(AbstractUser):
-    profile_pic = ProcessedImageField(
-        upload_to='static/images/profiles',
-        format='JPEG',
-        options={'quality':100},
-        blank=True,
-        null=True
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
         )
+
+    class Meta:
+        unique_together = ("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
